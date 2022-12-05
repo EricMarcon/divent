e040108ad6f6f3c741c2ac7aee41eae6765c412b
Eric Marcon <eric.marcon@agroparistech.fr>
2022-12-05 11:57:26 +0100

    Tests for ent_shannon and ent_simpson

---
+++ tests/testthat/test_ent_shannon.R
@@ -0,0 +1,120 @@
+# Combine all parameters
+abundances <- paracou_6_abd[1, ]
+
+testthat::test_that(
+  "No estimator fails", {
+    # Estimate diversity systematically
+    ent_shannon.list <- lapply(
+      # All estimators
+      eval(formals(divent:::ent_shannon.numeric)$estimator), 
+      function(estimator) {
+        the_list <-lapply(
+          # All probability estimators
+          eval(formals(divent:::ent_shannon.numeric)$probability_estimator), 
+          function(probability_estimator) {
+            the_list <- lapply(
+              # All unveilings
+              eval(formals(divent:::ent_shannon.numeric)$unveiling), 
+              function(unveiling) {
+                # print(paste(estimator, probability_estimator, unveiling))
+                suppressWarnings(
+                  ent_shannon(
+                    abundances,
+                    estimator = estimator,
+                    jack_alpha = 0.05,
+                    jack_max = 10,
+                    level = NULL,
+                    probability_estimator = probability_estimator,
+                    unveiling = unveiling,
+                    as_numeric = FALSE,
+                    check_arguments = TRUE
+                  )
+                )
+              }
+            ) 
+            # Make a dataframe with the list to avoid nested lists
+            the_df <- do.call(rbind, the_list)
+          }
+        )
+        # Make a dataframe with the list to avoid nested lists
+        the_df <- do.call(rbind, the_list)
+      }
+    )
+    # Coerce to a dataframe
+    ent_shannon.dataframe <- do.call(rbind, ent_shannon.list)
+    
+    # The min value must be UnveilJ / Chao2013 without unveiling
+    testthat::expect_equal(
+      min(ent_shannon.dataframe$entropy),
+      ent_shannon(
+        abundances, 
+        probability_estimator = "Chao2013",
+        unveiling = "none"
+      )$entropy
+    )
+  }
+)
+
+
+testthat::test_that(
+  "No estimator fails during interpolation and extrapolation", {
+    # Estimate diversity systematically
+    ent_shannon.list <- lapply(
+      # All estimators
+      eval(formals(divent:::ent_shannon.numeric)$estimator), 
+      function(estimator) {
+        the_list <-lapply(
+          # All probability estimators
+          eval(formals(divent:::ent_shannon.numeric)$probability_estimator), 
+          function(probability_estimator) {
+            the_list <- lapply(
+              # All unveilings
+              eval(formals(divent:::ent_shannon.numeric)$unveiling), 
+              function(unveiling) {
+                the_list <-lapply(
+                  # All levels
+                  levels, 
+                  function(level) {
+                    # print(paste(estimator, probability_estimator, unveiling, level))
+                    suppressWarnings(
+                      ent_shannon(
+                        abundances,
+                        estimator = estimator,
+                        jack_alpha = 0.05,
+                        jack_max = 10,
+                        level = level,
+                        probability_estimator = probability_estimator,
+                        unveiling = unveiling,
+                        as_numeric = FALSE,
+                        check_arguments = TRUE
+                      )
+                    )
+                  }
+                )
+                # Make a dataframe with the list to avoid nested lists
+                the_df <- do.call(rbind, the_list)
+              }
+            )
+            # Make a dataframe with the list to avoid nested lists
+            the_df <- do.call(rbind, the_list)
+          }
+        )
+        # Make a dataframe with the list to avoid nested lists
+        the_df <- do.call(rbind, the_list)
+      }
+    )
+    # Coerce to a dataframe
+    ent_shannon.dataframe <- do.call(rbind, ent_shannon.list)
+    
+    # The min value must be UnveilJ / Chao2013 without unveiling
+    testthat::expect_equal(
+      min(ent_shannon.dataframe$entropy),
+      ent_shannon(
+        abundances, 
+        probability_estimator = "Chao2013",
+        unveiling = "none",
+        level = 1413
+      )$entropy
+    )
+  }
+)
---
+++ tests/testthat/test_ent_simpson.R
@@ -0,0 +1,123 @@
+# Combine all parameters
+abundances <- paracou_6_abd[1, ]
+
+testthat::test_that(
+  "No estimator fails", {
+    # Estimate diversity systematically
+    ent_simpson.list <- lapply(
+      # All estimators
+      eval(formals(divent:::ent_simpson.numeric)$estimator), 
+      function(estimator) {
+        the_list <-lapply(
+          # All probability estimators
+          eval(formals(divent:::ent_simpson.numeric)$probability_estimator), 
+          function(probability_estimator) {
+            the_list <- lapply(
+              # All unveilings
+              eval(formals(divent:::ent_simpson.numeric)$unveiling), 
+              function(unveiling) {
+                # print(paste(estimator, probability_estimator, unveiling))
+                suppressWarnings(
+                  ent_simpson(
+                    abundances,
+                    estimator = estimator,
+                    jack_alpha = 0.05,
+                    jack_max = 10,
+                    level = NULL,
+                    probability_estimator = probability_estimator,
+                    unveiling = unveiling,
+                    as_numeric = FALSE,
+                    check_arguments = TRUE
+                  )
+                )
+              }
+            ) 
+            # Make a dataframe with the list to avoid nested lists
+            the_df <- do.call(rbind, the_list)
+          }
+        )
+        # Make a dataframe with the list to avoid nested lists
+        the_df <- do.call(rbind, the_list)
+      }
+    )
+    # Coerce to a dataframe
+    ent_simpson.dataframe <- do.call(rbind, ent_simpson.list)
+    
+    # The min value must be UnveilJ / Chao2013 without unveiling
+    testthat::expect_equal(
+      min(ent_simpson.dataframe$entropy),
+      ent_simpson(
+        abundances, 
+        estimator = "UnveilC",
+        probability_estimator = "Chao2013",
+        unveiling = "none"
+      )$entropy
+    )
+  }
+)
+
+
+testthat::test_that(
+  "No estimator fails during interpolation and extrapolation", {
+    # Estimate diversity systematically
+    ent_simpson.list <- lapply(
+      # All estimators
+      eval(formals(divent:::ent_simpson.numeric)$estimator), 
+      function(estimator) {
+        the_list <-lapply(
+          # All probability estimators
+          eval(formals(divent:::ent_simpson.numeric)$probability_estimator), 
+          function(probability_estimator) {
+            the_list <- lapply(
+              # All unveilings
+              eval(formals(divent:::ent_simpson.numeric)$unveiling), 
+              function(unveiling) {
+                the_list <-lapply(
+                  # All levels
+                  levels, 
+                  function(level) {
+                    # print(paste(estimator, probability_estimator, unveiling, level))
+                    suppressWarnings(
+                      ent_simpson(
+                        abundances,
+                        estimator = estimator,
+                        jack_alpha = 0.05,
+                        jack_max = 10,
+                        level = level,
+                        probability_estimator = probability_estimator,
+                        unveiling = unveiling,
+                        as_numeric = FALSE,
+                        check_arguments = TRUE
+                      )
+                    )
+                  }
+                )
+                # Make a dataframe with the list to avoid nested lists
+                the_df <- do.call(rbind, the_list)
+              }
+            )
+            # Make a dataframe with the list to avoid nested lists
+            the_df <- do.call(rbind, the_list)
+          }
+        )
+        # Make a dataframe with the list to avoid nested lists
+        the_df <- do.call(rbind, the_list)
+      }
+    )
+    # Coerce to a dataframe
+    ent_simpson.dataframe <- do.call(rbind, ent_simpson.list)
+    
+    # The min value must be at level 471, no matter the arguments
+    testthat::expect_equal(
+      min(ent_simpson.dataframe$entropy),
+      as.numeric(
+        unique(
+          ent_simpson.dataframe[
+            ent_simpson.dataframe$level == 471,
+            "entropy"
+          ]
+        )
+      )
+    )
+  }
+)
