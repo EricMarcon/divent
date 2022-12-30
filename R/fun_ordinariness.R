c08dd569e6a8ded0a691ec75be4f37365e1be6e1
Eric Marcon <eric.marcon@agroparistech.fr>
2022-12-30 17:10:20 +0100

    Similarity and ordinariness

---
+++ R/fun_ordinariness.R
@@ -0,0 +1,49 @@
+#' Functional ordinariness of a communitu
+#' 
+#' TODO
+#'
+#' @inheritParams check_divent_args
+#' @param distribution TODO
+#' @param similarities TODO
+#'
+#' @return A tibble with the ordinariness of each species.
+#' @export
+#'
+#' @examples
+#' fun_ordinariness(paracou_6_abd, Z)
+#' 
+fun_ordinariness <- function (
+    distribution,
+    similarities,
+    as_numeric = FALSE,
+    check_arguments = TRUE) {
+  
+  if (any(check_arguments)) check_divent_args()
+  
+  # Species names
+  is_species_column <- !(colnames(distribution) %in% non_species_columns)
+  species_names <- col_names[is_species_column]
+  if (length(setdiff(species_names, colnames(similarities))) != 0) {
+    stop("Some species are missing in the similarity matrix.")    
+  } else {
+    # Filter and reorder the similarity matrix
+    similarities <- similarities[species_names, species_names]
+  }
+  
+  # Calculate species probabilities
+  prob <- probabilities(distribution)[, is_species_column]
+  
+  # Calculate ordinariness
+  the_similarities <- as.matrix(prob) %*% t(similarities)
+  
+  if (as_numeric) {
+    return(the_similarities)
+  } else {
+    return(
+      tibble::tibble(
+        distribution[, !is_species_column],
+        as.data.frame(the_similarities)
+      )
+    )
+  }
+}
---
+++ R/fun_similarity.R
@@ -0,0 +1,50 @@
+#' Functional similarity
+#' 
+#' Transform a distance matrix into a similarity matrix.
+#'
+#' @inheritParams check_divent_args
+#' @param distances TODO
+#' @param exponential If `TRUE`, similarity is \eqn{e^{-r \delta}}, 
+#' where \eqn{r} is argument `rate`.
+#' If `FALSE`, it is \eqn{1 - \delta / \max(\delta)}.
+#' @param rate The decay rate of the exponential similarity.
+#'
+#' @return A similarity matrix.
+#' @export
+#'
+#' @examples
+#' # TODO
+#' 
+fun_similarity <- function (
+    distances,
+    exponential = TRUE,
+    rate = 1,
+    check_arguments = TRUE) {
+  
+  if (any(check_arguments)) check_divent_args()
+  
+  # Names
+  if (xor(is.null(colnames(distances)), is.null(rownames(distances)))) {
+    stop("Row and column names must be identical in 'distances'.")
+  }
+  if (is.null(colnames(distances))) {
+    ### Add default species names such as sp_1
+    colnames(x) <- paste(
+      "sp", 
+      formatC(seq_len(ncol(x)), width = ceiling(log10(ncol(x))), flag = "0"),
+      sep = "_"
+    )
+    rownames(x) <- colnames(x)
+  } else if (identical(colnames(distances), rownames(distances))) {
+    stop("Row and column names must be identical in 'distances'.")
+  }
+  
+  if (exponential) {
+    the_similarity <- 1 - distances / max(distances)
+  } else {
+    the_similarity <- exp(-rate * distances)
+  }
+  
+  class(the_similarity) <- c("similarity", class(the_similarity))
+  return(the_similarity)
+}
