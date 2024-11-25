edeaec22e70b3b28d9f47e9ff361ac3cd043a049
Eric Marcon <eric.marcon@agroparistech.fr>
2024-11-25 11:55:39 +0100

    assumed abundances are real values (not int)

--- R/species_distribution.R
+++ R/species_distribution.R
@@ -1,49 +1,49 @@
 #' Species Distributions
-#' 
-#' A Species Distribution is a [tibble::tibble] containing species abundances 
+#'
+#' A Species Distribution is a [tibble::tibble] containing species abundances
 #' or probabilities.
 
-#' Rows of the tibble are communities and column are species. 
-#' Values are either abundances or probabilities. 
-#' Special columns contain the site names, and their weights 
+#' Rows of the tibble are communities and column are species.
+#' Values are either abundances or probabilities.
+#' Special columns contain the site names, and their weights
 #' (e.g. their area or number of individuals):
-#' their names must be "site" and "weight". 
+#' their names must be "site" and "weight".
 #' All other column names are considered as species names.
-#' 
-#' `species_distribution` objects include `abundances` and `probabilities` 
+#'
+#' `species_distribution` objects include `abundances` and `probabilities`
 #' objects.
-#' 
-#' `species_distribution()` creates a `species_distribution` object from a vector 
+#'
+#' `species_distribution()` creates a `species_distribution` object from a vector
 #' or a matrix or a dataframe.
-#' 
-#' `as_species_distribution()`, `as_abundances()` and `as_probabilities` format 
-#' the numeric, matrix or dataframe `x` so that appropriate 
-#' versions of community functions (generic methods such as [plot] or 
-#' [div_richness]) are applied. 
+#'
+#' `as_species_distribution()`, `as_abundances()` and `as_probabilities` format
+#' the numeric, matrix or dataframe `x` so that appropriate
+#' versions of community functions (generic methods such as [plot] or
+#' [div_richness]) are applied.
 #' Abundance values are rounded (by default) to the nearest integer.
-#' They also accept a [dbmss::wmppp] objects, 
-#' i.e. a weighted, marked planar point pattern and count the abundances of 
+#' They also accept a [dbmss::wmppp] objects,
+#' i.e. a weighted, marked planar point pattern and count the abundances of
 #' point types, character and factor objects.
-#' 
+#'
 #' `as_probabilities()` normalizes the vector `x` so that it sums to 1. It gives
 #' the same output as `probabilities()` with `estimator = "naive"`.
-#' 
+#'
 #' `species_distribution` objects objects can be plotted by [plot] and [autoplot].
 #'
 #' @inheritParams check_divent_args
 #' @param x an object.
 #' @param ... Unused.
 
-#' @returns An object of classes `species_distribution` and `abundances` 
+#' @returns An object of classes `species_distribution` and `abundances`
 #' or `probabilities`.
-#' 
-#' `as.double()` and its synonymous `as.numeric()` return a numeric vector 
-#' that contains species abundances or probabilities of a single-row 
-#' `species_distribution`. 
-#' `as.matrix()` returns a numeric matrix if the `species_distribution` contains 
+#'
+#' `as.double()` and its synonymous `as.numeric()` return a numeric vector
+#' that contains species abundances or probabilities of a single-row
+#' `species_distribution`.
+#' `as.matrix()` returns a numeric matrix if the `species_distribution` contains
 #' several rows.
 #' These are methods of the generic functions for class `species_distribution`.
-#' 
+#'
 #' @examples
 #' # Paracou data is a tibble
 #' paracou_6_abd
@@ -54,7 +54,7 @@
 #' autoplot(paracou_6_abd[1,], fit_rac = TRUE, distribution = "lnorm")
 #' # Character vectors
 #' as_abundances(c("A", "C", "B", "C"))
-#' 
+#'
 #' @name species_distribution
 NULL
 
@@ -64,12 +64,12 @@ NULL
 #' @rdname species_distribution
 #'
 #' @param names The names of the species distributions.
-#' 
+#'
 #' @export
 species_distribution <- function(
-    x, 
-    names = NULL, 
-    weights = NULL, 
+    x,
+    names = NULL,
+    weights = NULL,
     check_arguments = TRUE) {
 
   # Check the data ----
@@ -86,7 +86,7 @@ species_distribution <- function(
     if (is.null(names(x))) {
       ### Columns: add default species names such as sp_1 ----
       names(x) <- paste(
-        "sp", 
+        "sp",
         formatC(seq_along(x), width = ceiling(log10(length(x))), flag = "0"),
         sep = "_"
       )
@@ -106,7 +106,7 @@ species_distribution <- function(
     if (is.null(colnames(x))) {
       ### Columns: add default species names such as sp_1 ----
       colnames(x) <- paste(
-        "sp", 
+        "sp",
         formatC(seq_len(ncol(x)), width = ceiling(log10(ncol(x))), flag = "0"),
         sep = "_"
       )
@@ -126,7 +126,7 @@ species_distribution <- function(
       if (is.null(row.names(x))) {
         # ...and no row names: set default names such as site_1
         the_distribution$site <- paste(
-          "site", 
+          "site",
           formatC(seq_len(nrow(x)), width = ceiling(log10(nrow(x))), flag = "0"),
           sep = "_"
         )
@@ -137,7 +137,7 @@ species_distribution <- function(
       # site = weights if the size matches
       if (length(weights) == nrow(x)) {
         the_distribution <- tibble::add_column(
-          the_distribution, 
+          the_distribution,
           weight = weights,
           .after = "site"
         )
@@ -147,13 +147,13 @@ species_distribution <- function(
     } else {
       # Weights are the number of individuals
       the_distribution <- tibble::add_column(
-        the_distribution, 
+        the_distribution,
         weight = rowSums(x),
         .after = "site"
       )
     }
   }
-  
+
   # Set the class and return ----
   class(the_distribution) <- c("species_distribution", class(the_distribution))
   return(the_distribution)
@@ -175,7 +175,7 @@ as_species_distribution.numeric <- function(
     x,
     ...,
     check_arguments = TRUE) {
-  
+
   return(
     species_distribution(
       x,
@@ -189,12 +189,12 @@ as_species_distribution.numeric <- function(
 #'
 #' @export
 as_species_distribution.matrix <- function(
-    x, 
-    names = NULL, 
-    weights = NULL, 
+    x,
+    names = NULL,
+    weights = NULL,
     ...,
     check_arguments = TRUE) {
-  
+
   return(
     species_distribution(
       x,
@@ -211,25 +211,25 @@ as_species_distribution.matrix <- function(
 #' @export
 as_species_distribution.data.frame <- function(
     x,
-    ..., 
+    ...,
     check_arguments = TRUE) {
-  
+
   if (any(check_arguments)) check_divent_args()
   # Check the data
   if (any(x < 0)) stop("All numeric values of the dataframe must be positive.")
-  
+
   # Build a tibble
   the_distribution <- tibble::as_tibble(x)
-  
+
   # The first column should be "site"
   if (!"site" %in% colnames(the_distribution)) {
     the_distribution <- tibble::add_column(
-      the_distribution, 
+      the_distribution,
       site = paste(
-        "site", 
+        "site",
         formatC(
-          seq_len(nrow(the_distribution)), 
-          width = ceiling(log10(nrow(the_distribution))), 
+          seq_len(nrow(the_distribution)),
+          width = ceiling(log10(nrow(the_distribution))),
           flag = "0"
         ),
         sep = "_"
@@ -237,11 +237,11 @@ as_species_distribution.data.frame <- function(
       .before = 1
     )
   }
-  
+
   # The second column should be "weight"
   if (!"weight" %in% colnames(the_distribution)) {
     the_distribution <- tibble::add_column(
-      the_distribution, 
+      the_distribution,
       weight = rowSums(the_distribution[colnames(the_distribution) != "site"]),
       .after = "site"
     )
@@ -258,9 +258,9 @@ as_species_distribution.data.frame <- function(
 #' @export
 as_species_distribution.wmppp <- function(
     x,
-    ..., 
+    ...,
     check_arguments = TRUE) {
-  
+
   return(
     species_distribution(
       as_named_vector.wmppp(x),
@@ -275,9 +275,9 @@ as_species_distribution.wmppp <- function(
 #' @export
 as_species_distribution.character <- function(
     x,
-    ..., 
+    ...,
     check_arguments = TRUE) {
-  
+
   # Count the number of items by type
   return(
     species_distribution(
@@ -293,13 +293,13 @@ as_species_distribution.character <- function(
 #' @export
 as_species_distribution.factor <- function(
     x,
-    ..., 
+    ...,
     check_arguments = TRUE) {
-  
+
   # Count the number of items by type
   return(
     species_distribution(
-      # tapply keep factors' names 
+      # tapply keep factors' names
       tapply(x, x, length) ,
       check_arguments = check_arguments
     )
@@ -331,7 +331,7 @@ as_probabilities <- function(x, ...) {
 #' @export
 as_probabilities.numeric <- function(
     x,
-    ..., 
+    ...,
     check_arguments = TRUE) {
 
   if (any(x < 0)) stop("Species probabilities must be positive.")
@@ -349,7 +349,7 @@ as_probabilities.numeric <- function(
 
 
 #' @rdname species_distribution
-#' 
+#'
 #' @export
 as_probabilities.matrix <- function(
     x,
@@ -357,18 +357,18 @@ as_probabilities.matrix <- function(
     weights = NULL,
     ...,
     check_arguments = TRUE) {
-  
+
   # Calculate probabilities by row
   prob <- x / rowSums(x)
-  
+
   # Build the species distribution
   the_probabilities <- as_species_distribution.matrix(
-    prob, 
-    names = names, 
+    prob,
+    names = names,
     weights = weights,
     check_arguments = check_arguments
   )
-  
+
   # Set the class
   class(the_probabilities) <- c("probabilities", class(the_probabilities))
   return(the_probabilities)
@@ -379,16 +379,16 @@ as_probabilities.matrix <- function(
 #'
 #' @export
 as_probabilities.data.frame <- function(
-    x, 
+    x,
     ...,
     check_arguments = TRUE) {
-  
+
   # Build the species distribution to add site and weight columns if needed
   abundances <- as_species_distribution.data.frame(
-    x, 
+    x,
     check_arguments = check_arguments
   )
-  
+
   # Select species columns
   species_columns <- !colnames(abundances) %in% non_species_columns
   # Extract abundances
@@ -402,13 +402,13 @@ as_probabilities.data.frame <- function(
   )
   # Restore exact species names (spaces may have been transformed into "_")
   colnames(the_probabilities[, species_columns]) <- colnames(abundances[, species_columns])
-  
+
   # Build the species distribution again for the classes
   the_probabilities <- as_species_distribution.data.frame(
     the_probabilities,
     check_arguments = FALSE
   )
-  
+
   # Set the class
   class(the_probabilities) <- c("probabilities", class(the_probabilities))
   return(the_probabilities)
@@ -420,14 +420,14 @@ as_probabilities.data.frame <- function(
 #' @export
 as_probabilities.wmppp <- function(
     x,
-    ..., 
+    ...,
     check_arguments = TRUE) {
-  
+
   the_probabilities <- as_species_distribution(
     as_named_vector.wmppp(x),
     check_arguments = check_arguments
   )
-  
+
   class(the_probabilities) <- c("probabilities", class(the_probabilities))
   return(the_probabilities)
 }
@@ -438,14 +438,14 @@ as_probabilities.wmppp <- function(
 #' @export
 as_probabilities.character <- function(
     x,
-    ..., 
+    ...,
     check_arguments = TRUE) {
-  
+
   the_probabilities <- as_species_distribution(
     as_named_vector.character(x),
     check_arguments = check_arguments
   )
-  
+
   class(the_probabilities) <- c("probabilities", class(the_probabilities))
   return(the_probabilities)
 }
@@ -456,22 +456,22 @@ as_probabilities.character <- function(
 #' @export
 as_probabilities.factor <- function(
     x,
-    ..., 
+    ...,
     check_arguments = TRUE) {
-  
+
   the_probabilities <- as_species_distribution(
-    # tapply keep factors' names 
+    # tapply keep factors' names
     tapply(x, x, length) ,
     check_arguments = check_arguments
   )
-  
+
   class(the_probabilities) <- c("probabilities", class(the_probabilities))
   return(the_probabilities)
 }
 
 
 #' @rdname species_distribution
-#' 
+#'
 #' @export
 is_probabilities <- function(x) {
   inherits(x, "probabilities")
@@ -482,16 +482,16 @@ is_probabilities <- function(x) {
 
 #' @rdname species_distribution
 #'
-#' @param round If `TRUE`, the values of `x` are converted to integers.
-#' 
+#' @param round If `TRUE`, the values of `x` are rounded to the nearest integer.
+#'
 #' @export
 abundances <- function(
     x,
     round = TRUE,
-    names = NULL, 
-    weights = NULL, 
+    names = NULL,
+    weights = NULL,
     check_arguments = TRUE) {
-  
+
   if (any(check_arguments)) {
     check_divent_args()
     if (!is.numeric(x)) stop("'x' must be numeric")
@@ -499,24 +499,22 @@ abundances <- function(
   }
 
   if (round) {
-    # Add 0.5 before changing mode to round rather than taking the floor
-    x <- x + 0.5
-    mode(x) <- "integer"
+    x <- round(x)
   }
-  
+
   abundances <- species_distribution(
-    x,     
-    names = names, 
-    weights = weights, 
+    x,
+    names = names,
+    weights = weights,
     check_arguments = FALSE
   )
 
   class(abundances) <- c("abundances", class(abundances))
-  return(abundances)    
+  return(abundances)
 }
 
 #' @rdname species_distribution
-#' 
+#'
 #' @export
 as_abundances <- function(x, ...) {
   UseMethod("as_abundances")
@@ -524,11 +522,11 @@ as_abundances <- function(x, ...) {
 
 
 #' @rdname species_distribution
-#' 
+#'
 #' @export
 as_abundances.numeric <- function(
     x,
-    round = TRUE, 
+    round = TRUE,
     ...,
     check_arguments = TRUE) {
 
@@ -539,7 +537,7 @@ as_abundances.numeric <- function(
 
   if (round) {
     the_names <- names(x)
-    x <- as.integer(round(x))
+    x <- round(x)
     names(x) <- the_names
   }
 
@@ -551,7 +549,7 @@ as_abundances.numeric <- function(
 
 
 #' @rdname species_distribution
-#' 
+#'
 #' @export
 as_abundances.matrix <- function(
     x,
@@ -560,20 +558,18 @@ as_abundances.matrix <- function(
     weights = NULL,
     ...,
     check_arguments = TRUE) {
-  
+
   if (round) {
-    # Add 0.5 before changing mode to round rather than taking the floor
-    x <- x + 0.5
-    mode(x) <- "integer"
+    x <- round(x)
   }
-  
+
   the_abundances <- as_species_distribution.matrix(
     x,
     names = names,
     weights = weights,
     check_arguments = check_arguments
   )
-  
+
   class(the_abundances) <- c("abundances", class(the_abundances))
   return(the_abundances)
 }
@@ -583,77 +579,77 @@ as_abundances.matrix <- function(
 #'
 #' @export
 as_abundances.data.frame <- function(
-    x, 
+    x,
     ...,
     check_arguments = TRUE) {
-  
+
   the_abundances <- as_species_distribution.data.frame(
     x,
     check_arguments = check_arguments
   )
-  
+
   class(the_abundances) <- c("abundances", class(the_abundances))
   return(the_abundances)
 }
 
 
 #' @rdname species_distribution
-#' 
+#'
 #' @export
 as_abundances.wmppp <- function(
     x,
     ...,
     check_arguments = TRUE) {
-  
+
   the_abundances <- as_species_distribution(
     as_named_vector.wmppp(x),
     check_arguments = check_arguments
   )
-  
+
   class(the_abundances) <- c("abundances", class(the_abundances))
   return(the_abundances)
 }
 
 
 #' @rdname species_distribution
-#' 
+#'
 #' @export
 as_abundances.character <- function(
     x,
     ...,
     check_arguments = TRUE) {
-  
+
   the_abundances <- as_species_distribution(
     as_named_vector.character(x),
     check_arguments = check_arguments
   )
-  
+
   class(the_abundances) <- c("abundances", class(the_abundances))
   return(the_abundances)
 }
 
 
 #' @rdname species_distribution
-#' 
+#'
 #' @export
 as_abundances.factor <- function(
     x,
     ...,
     check_arguments = TRUE) {
-  
+
   the_abundances <- as_species_distribution(
-    # tapply keep factors' names 
+    # tapply keep factors' names
     tapply(x, x, length) ,
     check_arguments = check_arguments
   )
-  
+
   class(the_abundances) <- c("abundances", class(the_abundances))
   return(the_abundances)
 }
 
 
 #' @rdname species_distribution
-#' 
+#'
 #' @export
 is_abundances <- function(x) {
   inherits(x, "abundances")
@@ -663,10 +659,10 @@ is_abundances <- function(x) {
 #  Opposite conversions ----
 
 #' @rdname species_distribution
-#' 
-#' @param use.names If `TRUE`, the names of the `species_distribution` are kept 
+#'
+#' @param use.names If `TRUE`, the names of the `species_distribution` are kept
 #' in the matrix or vector they are converted to.
-#' 
+#'
 #' @export
 as.matrix.species_distribution <- function(x, use.names = TRUE, ...) {
   # Delete co
@@ -679,7 +675,7 @@ as.matrix.species_distribution <- function(x, use.names = TRUE, ...) {
 
 
 #' @rdname species_distribution
-#' 
+#'
 #' @export
 as.double.species_distribution <- function(x, use.names = TRUE, ...) {
   if (nrow(x) > 1) {
@@ -688,13 +684,13 @@ as.double.species_distribution <- function(x, use.names = TRUE, ...) {
   } else {
     is_species_column <- !colnames(x) %in% non_species_columns
     the_vector <- unlist(x[1, is_species_column], use.names = use.names)
-    return(the_vector) 
+    return(the_vector)
   }
 }
 
 
 #' @rdname species_distribution
-#' 
+#'
 #' @export
 as.numeric.species_distribution <- function(x, use.names = TRUE, ...) {
   return(as.double.species_distribution(x, use.names, ...))
@@ -704,9 +700,9 @@ as.numeric.species_distribution <- function(x, use.names = TRUE, ...) {
 #  Utilities ----
 
 #' as_named_vector.character
-#' 
+#'
 #' Counts the number of points of a `character` vector and returns a named vector.
-#' Names are the items of the character vector. 
+#' Names are the items of the character vector.
 #' This is equivalent to `as.numeric(table(x))` but `table()`
 #' looses the names.
 #'
@@ -720,15 +716,15 @@ as_named_vector.character <- function(x){
   the_array <- tapply(x, x, length)
   the_vector <- as.vector(the_array)
   # Add the names
-  names(the_vector) <- names(the_array) 
+  names(the_vector) <- names(the_array)
   return(the_vector)
 }
 
 
 #' as_named_vector.wmppp
-#' 
+#'
 #' Counts the number of points of a `wmppp` object and returns a named vector.
-#' Names are the point types. 
+#' Names are the point types.
 #' This is equivalent to `as.numeric(table(X$marks$PointType))` but `table()`
 #' looses the names.
 #'
