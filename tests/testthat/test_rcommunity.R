ced95904cfb80da655b94c282f4182b54855778c
Eric Marcon <eric.marcon@agroparistech.fr>
2026-09-18 09:56:37 +0200

    corrected typo

--- tests/testthat/test_rcommunity.R
+++ tests/testthat/test_rcommunity.R
@@ -5,7 +5,7 @@ testthat::test_that(
     # Simulate communities systematically
     rcommunity.list <- lapply(
       # All distributions
-      eval(formals(divent:::rcommunity)$distribution), 
+      eval(formals(divent:::rcommunity)$distribution),
       function(distribution) {
         # print(distribution)
         rcommunity(
@@ -16,7 +16,7 @@ testthat::test_that(
           check_arguments = TRUE
         )
       }
-    ) 
+    )
 
     # Coerce to a dataframe
     rcommunity.dataframe <- dplyr::bind_rows(rcommunity.list)
@@ -27,12 +27,12 @@ testthat::test_that(
         .cols = dplyr::everything(),
         .fns = ~ ifelse(is.na(.x), 0, .x))
     )
-    
-    
+
+
     # The number of species must be less than 300
     testthat::expect_gte(
       300,
-      ncol(abd_species(rcommunity.dataframe)),
+      ncol(abd_species(rcommunity.dataframe))
     )
   }
 )
@@ -46,7 +46,7 @@ testthat::test_that(
     # Simulate communities systematically
     rcommunity.list <- lapply(
       # All distributions
-      eval(formals(divent:::rcommunity)$bootstrap), 
+      eval(formals(divent:::rcommunity)$bootstrap),
       function(bootstrap) {
         # print(bootstrap)
         rcommunity(
@@ -56,8 +56,8 @@ testthat::test_that(
           check_arguments = TRUE
         )
       }
-    ) 
-    
+    )
+
     # Coerce to a dataframe
     rcommunity.dataframe <- dplyr::bind_rows(rcommunity.list)
     # Replace NA's due to binding by zeros
@@ -67,8 +67,8 @@ testthat::test_that(
         .cols = dplyr::everything(),
         .fns = ~ ifelse(is.na(.x), 0, .x))
     )
-    
-    
+
+
     # The number of individuals must equal the sample size
     testthat::expect_gte(
       unique(rcommunity.dataframe$weight),
